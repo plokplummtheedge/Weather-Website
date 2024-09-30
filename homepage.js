7d7935156a3f01f1d7abcc9a87179cca781ca809
plokplummtheedge <151432916+plokplummtheedge@users.noreply.github.com>
2024-09-30 20:07:05 +0800

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,33 @@
+const apiKey = '69e54be3b3d041ca9be9a6c79d713ffb';
+const url = `https://newsapi.org/v2/everything?q=typhoon&apiKey=${apiKey}`;
+
+async function fetchNews() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        displayNews(data.articles);
+    } catch (error) {
+        console.error('Error fetching the news:', error);
+    }
+}
+
+function displayNews(articles) {
+    const newsContainer = document.getElementById('news-container');
+    articles.forEach(article => {
+        const articleElement = document.createElement('div');
+        articleElement.classList.add('article');
+        articleElement.style.marginBottom = '40px';
+        articleElement.innerHTML = `
+            <h2>${article.title}</h2>
+            <img src="${article.urlToImage}" alt="${article.title}" style="width:100%; height:auto; max-width:600px;">
+            <p>${article.description}</p>
+            <a href="${article.url}" target="_blank">Read more</a>
+        `;
+        newsContainer.appendChild(articleElement);
+    });
+}
+
+fetchNews();
+
+
+
